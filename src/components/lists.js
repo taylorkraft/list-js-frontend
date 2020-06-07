0ea4f217565faed40dce448643269925dd98d12d
taylorkraft <taylorkraft16@gmail.com>
2020-06-07 15:00:24 -0600

    update methods

--- src/components/lists.js
+++ src/components/lists.js
@@ -10,16 +10,22 @@ class Lists {
     this.adapter
     .getLists()
     .then(lists => {
-      return console.log(lists)
+      lists.forEach(list => this.lists.push(list))
+      //as we iterate over that array of objects(lists) we're going 
+      //to push that individual object(list) into this.lists = []
+      //now we have access to the array of lists in other methods
+      //by calling this.notes
     })
     .then(() => {
       this.render()
+      //once we get lists we call render()
     })
   }
-//once we get lists we call render()
+
 
   render() {
     const listsContainer = document.getElementById('lists-container')
+    //grab our div from index.html 
     listsContainer.innerHTML = "my lists"
   }
 }
